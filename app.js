bfa03070e1799dbded90cdbb48c081f6a79c1d45
Rory <rory88@gmail.com>
2016-01-20 18:51:45 +0000

    added enemies. refactored some parts. general debugging of the map

--- app.js
+++ app.js
@@ -1,10 +1,25 @@
+// THINGS TO BEAR IN MIND.
+// Enemies not yet implemented.
+// Boss.
+// Viewport.
+
+// Problematic bottom line of table.
+// Goal sometimes renders in front of other stuff in corridor (other stuff is unreachable).
+// Sometimes an enemy spawns on the character
+
 let Main = React.createClass({
 
    getInitialState() {
       let cols = 25;
-      let rows = 35;
+      let rows = 25;
       let grid = [];
+      let char = {
+         health: 100,
+         xp: 0,
+         weapon: 'rusty dagger'
+      };
 
+      let level = 1;
 
       let charCoords = [Math.floor(rows / 2), Math.floor(cols / 2)];
 
@@ -14,7 +29,7 @@ let Main = React.createClass({
          enemies: [],
          blocks: [],
          paths: [],
-         goal: []
+         goal: [],
       };
 
       _.times(rows, (i) => { 
@@ -25,36 +40,15 @@ let Main = React.createClass({
          });
       });
 
-      return {rows, cols, grid, charCoords, generatedCells};
+      return {rows, cols, grid, charCoords, generatedCells, char, level};
    },
 
    componentDidMount() {
       $(document.body).on('keydown', this.handleKeyDown);
       let {generatedCells, charCoords, cols, rows} = this.state;
       let {weapons, healths, enemies, blocks, paths, goal} = generatedCells;
-   
 
-      // weapons.push(
-      //    `${_.random(9, 10)}_${_.random(9, 10)}`
-      // );
-      // healths.push(
-      //    `${_.random(15, 20)}_${_.random(15, 13)}`,
-      //    `${_.random(7, 9)}_${_.random(7, 9)}`,
-      //    `${_.random(21, 23)}_${_.random(21, 24)}`
-      // );
-      // enemies.push(
-      //    '10_9',
-      //    '3_6',
-      //    '5_4'
-      // );
-
-      // safe Y = cols - 4
-      // safe x = rows - 3
-      // blocks.splice(blocks.indexOf(healths[1]), 1)
-      goal = '6_15'; // this is NOT working, it's being set from getInitialState
-
-      this.setState({generatedCells: generatedCells})
-      
+      this.forceUpdate();
       // set the map's playable path
       this.setPathsInMap();
    },
@@ -69,10 +63,10 @@ let Main = React.createClass({
       let homeCell = myCoords;
 
       let getRandomCoords = (a, b) => {
-         return `${_.random(a - 1, a + 1)}_${_.random(b - 1, b + 1)}`;
+         return `${_.random(a, a + 1)}_${_.random(b, b + 1)}`;
       }
 
-      let randomCoords = [
+      let cornerCoords = [
          getRandomCoords(Math.floor(cols / 10), Math.floor(rows / 10)), //top left
          getRandomCoords(Math.floor(cols / 10), cols - 6), // top right
          getRandomCoords(rows - 6, Math.floor(cols / 10)), // bottom left
@@ -83,67 +77,61 @@ let Main = React.createClass({
          getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 4
          getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 5
          getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 6
+         getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 6
+         getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 6
+         getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 6
+         getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 6
+         getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 6
+         getRandomCoords(_.random(0, rows - 4), _.random(0, cols - 4)), // random 6
       ];
 
-      console.log('topleft:', randomCoords[0])
-      console.log('topright:', randomCoords[1])
-      console.log('bottomleft:', randomCoords[2])
-      console.log('bottomright:', randomCoords[3])
-      console.log('random 1:', randomCoords[4])
-      console.log('random 2:', randomCoords[5])
-      console.log('random 3:', randomCoords[6])
+      console.log('topleft:', cornerCoords[0])
+      console.log('topright:', cornerCoords[1])
+      console.log('bottomleft:', cornerCoords[2])
+      console.log('bottomright:', cornerCoords[3])
+      console.log('random 1:', cornerCoords[4])
+      console.log('random 2:', cornerCoords[5])
+      console.log('random 3:', cornerCoords[6])
 
 
-      let sampleAndRemove = () => {
-         if(randomCoords.length == 0) {
+      let getNSamplesAndRemove = (num) => {
+         let res = [];
+
+         if(cornerCoords.length == 0) {
             console.log('ERROR: No items in coords left!')
             return;
          }
-         let sample = _.sample(randomCoords)
-         console.log(sample)
-         console.log('index:', randomCoords.indexOf(sample))
-         randomCoords.splice(randomCoords.indexOf(sample), 1);
-         return sample;
+
+         //TODO: If none left in cornerCoords, just generate new random coords here
+
+         _.times(num, () => {
+            let sample = _.sample(cornerCoords)
+            console.log(sample)
+            console.log('index:', cornerCoords.indexOf(sample))
+            cornerCoords.splice(cornerCoords.indexOf(sample), 1);
+            res.push(sample);
+         });
+
+         return res;
       }
 
       // could implement _.shuffle() here
 
-      weapons.push(
-         sampleAndRemove()
-      );
-         
-      healths.push(
-         sampleAndRemove(),
-         sampleAndRemove(),
-         sampleAndRemove(),
-      );
+      weapons.push(...getNSamplesAndRemove(1));
+      healths.push(...getNSamplesAndRemove(3));
+      goal.push(...getNSamplesAndRemove(1));
 
-      goal.push(sampleAndRemove());
+      let itemsToRender = _.union(weapons, healths, enemies, goal)
 
-      enemies.push(
-         '10_9',
-         '3_6',
-         '5_4'
+      let remainingTargets = [];
+      remainingTargets.push(
+         ...itemsToRender, // render weapons, healths, enemies, goal
+         ...getNSamplesAndRemove(Math.floor(cols / 5) - 1) // render some random points
       );
 
-      let remainingTargets = [
-         weapons[0], 
-         healths[0], 
-         healths[1], 
-         healths[2], 
-         goal[0],
-         sampleAndRemove(),
-         sampleAndRemove(),
-         sampleAndRemove(),
-         sampleAndRemove(),
-         sampleAndRemove(),
-      ];
-
-      // THINGS TO BEAR IN MIND.
-      // When there is an item on the map, it's moveable to as well.
-      // Enemies not yet implemented.
-      // Where should player begin map from.
-      // Viewport.
+      console.log('WEAPONS:', weapons)
+      console.log('HEALTHS:', healths)
+      console.log('REMAINING:', remainingTargets)
 
       remainingTargets.forEach((e,i) => {
 
@@ -190,13 +178,45 @@ let Main = React.createClass({
 
 
             currentCell = currentX + '_' + currentY;
-            count++; // increment for while loop
+            count++; // increment while loop
 
             // console.log('current after:', currentX, currentY)
          }
          this.carvePath(paths, blocks, currentCell); //if not called again here, it won't carve the very last cell
+         
+      })// end foreach
 
-      })
+      let renderNEnemies = (num) => {
+         
+         console.log('charCoords:', charCoords)
+
+         let res = [];
+
+         let shuffledPaths = paths.slice();
+
+         shuffledPaths = _.shuffle(shuffledPaths);
+
+         shuffledPaths.forEach((e, i) => {
+            if(_.includes(weapons, e) ||
+               _.includes(healths, e) ||
+               _.includes(goal, e) ||
+               e == `${charCoords[0]}_${charCoords[1]}`){
+               console.log(e)
+               shuffledPaths.splice(i, 1);
+            }
+         })
+         
+         _.times(num, (i) => {
+            res.push({coords: shuffledPaths[i], health: 10})
+         })
+
+         return res;
+      }
+      
+      // enemies must be rendered after the path has been rendered!
+      enemies.push(...renderNEnemies(4))
+
+      this.setState({generatedCells: generatedCells})
    },
 
    carvePath(paths, blocks, coords) {
@@ -216,40 +236,137 @@ let Main = React.createClass({
       $(document.body).off('keydown', this.handleKeyDown);
    },
 
+   reInit() {
+      let cols = 25;
+      let rows = 25;
+      let grid = [];
+      let char = {
+         health: 100,
+         xp: 0,
+         weapon: 'rusty dagger'
+      };
+
+      let level = 2;
+
+      let charCoords = [Math.floor(rows / 2), Math.floor(cols / 2)];
+
+      let generatedCells = {
+         weapons: [],
+         healths: [],
+         enemies: [],
+         blocks: [],
+         paths: [],
+         goal: [],
+      };
+
+      _.times(rows, (i) => { 
+         grid.push([]);
+         _.times(cols, (j) => { 
+            grid[i].push(' ');
+            generatedCells.blocks.push(`${i}_${j}`)
+         });
+      });
+
+      this.setState({
+         rows: rows,
+         cols: cols,
+         grid: grid,
+         char: char,
+         level: level,
+         charCoords: charCoords,
+         generatedCells: generatedCells,
+      })
+   },
+
+   attackEnemy(x, y){
+      let {char, generatedCells} = this.state;
+      let {enemies} = generatedCells;
+      
+      // identify the enemy from the state
+      let enemyToDamage = _.find(enemies, (e) => e.coords == `${x}_${y}`);
+      
+      // char attacked enemy
+      enemyToDamage.health -= 4;
+      char.xp += 150;
+
+      // check if enemy died
+      if(enemyToDamage.health <= 0){
+         enemies.splice(enemies.indexOf(enemyToDamage), 1)
+      }
+
+      // enemy attacked char
+      char.health -= 30;
+      this.setState({char: char})
+
+      // check if char died
+      if(char.health <= 0){
+         alert('You died!')
+         // died. re-render!
+         this.reInit(); //pass args here
+         this.setPathsInMap();
+         
+         // This needs to be called otherwise it doesn't update. I DON'T KNOW WHY! I tried setting a method that simply re-sets state but it failed.
+         // console says 'charCoords is not defined'. yet if i remove it it doesn't re-render. WTF!
+         this.setState({
+            charCoords: charCoords,
+            generatedCells: generatedCells
+         });        
+      }
+   },
+
    moveMyChar(direction) { // changes state
 
-      let {grid, charCoords, cols, generatedCells} = this.state;
+      let {grid, charCoords, cols, generatedCells, char} = this.state;
+      let {weapons, healths, goal, enemies, blocks} = generatedCells;//put this at top of method
+      let enemyCoords = [];
+
+      generatedCells.enemies.forEach((e, i) => {
+         enemyCoords.push(e.coords)
+      });
 
       let cc = charCoords.slice(); // for brevity
 
       let x = cc[0];
       let y = cc[1];
 
-      console.log('generatedCells.blocks:', generatedCells.blocks)
-
       let targetCellIsAvailable = (x, y) => {
-         console.log('generatedCells.blocks.indexOf(x_y):', generatedCells.blocks.indexOf(`${x}_${y}`))
+         return !_.includes(blocks, `${x}_${y}`) &&
+         !_.includes(enemyCoords,`${x}_${y}`);
+      };
 
-         return generatedCells.blocks.indexOf(`${x}_${y}`) == -1;
+      let targetCellIsEnemy = (x, y) => {
+         return _.includes(enemyCoords, `${x}_${y}`);
       };
 
       switch(direction) {
       case 'up':
-         if (x !== 0 && targetCellIsAvailable(x - 1, y)) {               // guard: not top of table & moveable to
+         if(targetCellIsEnemy(x - 1, y)){
+            this.attackEnemy(x - 1, y);
+         }
+         else if (x !== 0 && targetCellIsAvailable(x - 1, y)) {               // guard: not top of table & moveable to
             this.setNewCharLocation(cc, x, y, 0, direction);
          }
          break;
       case 'down':
-         if (x !== grid.length - 1 && targetCellIsAvailable(x + 1, y)) { // guard: not bottom of table & moveable to
+         if(targetCellIsEnemy(x + 1, y)){
+            this.attackEnemy(x + 1, y);
+         }
+         else if (x !== grid.length - 1 && targetCellIsAvailable(x + 1, y)) { // guard: not bottom of table & moveable to
             this.setNewCharLocation(cc, x, y, 0, direction);
          }
          break;
       case 'left':
+         if(targetCellIsEnemy(x, y - 1)){
+            this.attackEnemy(x, y - 1);
+         }
          if (y !== 0 && targetCellIsAvailable(x, y - 1)) {               // guard: not extreme left of table & moveable to
             this.setNewCharLocation(cc, x, y, 1, direction);
          }
          break;
       case 'right':
+         if(targetCellIsEnemy(x, y + 1)){
+            this.attackEnemy(x, y + 1);
+         }
          if (y !== cols - 1 && targetCellIsAvailable(x, y + 1)) {       // guard: not extreme right of table & moveable to
             this.setNewCharLocation(cc, x, y, 1, direction);
          }
@@ -258,27 +375,56 @@ let Main = React.createClass({
          break;
       }
 
-      charCoords = cc.slice();
-      console.log('charCoords after move:', charCoords)
-      let{weapons, healths} = generatedCells;
-      this.removeFromItemsIfNommed(weapons, charCoords, 'w');
-      this.removeFromItemsIfNommed(healths, charCoords, 'h');
+      if(`${cc[0]}_${cc[1]}` == generatedCells.goal[0]){
+         // goal reached. re-render!
+         this.reInit(); //pass args here
+         this.setPathsInMap();
+         this.forceUpdate();
+         
+      } else {
 
-      this.setState({
-         grid: grid,
-         charCoords: charCoords,
-         generatedCells: generatedCells
-      });
+         charCoords = cc.slice();
+         console.log('charCoords after move:', charCoords)
+         this.removeFromItemsIfNommed(weapons, charCoords, 'w');
+         this.removeFromItemsIfNommed(healths, charCoords, 'h');
+         this.removeFromItemsIfNommed(enemyCoords, charCoords, 'e');
+         
+         // This is what's called when dead, but if this is out of the else, it makes the port fail!
+         this.setState({
+            grid: grid,
+            charCoords: charCoords,
+            generatedCells: generatedCells
+         });
+      }
    },
 
    removeFromItemsIfNommed(arr, charCoords, name) {
+      
+      let {char} = this.state;
+
       arr.forEach((e, i) => {
          if(e.split('_')[0] == charCoords[0] && e.split('_')[1] == charCoords[1]){ // char is on weapon cell
-            arr.splice(i, 1) // delete from arr
             // log for debugging:
             switch(name) {
-            case 'w': console.log('nommed weapon'); break;
-            case 'h': console.log('nommed health'); break;
+            case 'w': 
+               console.log('nommed weapon');
+               arr.splice(i, 1) // delete from arr
+               char.weapon = 'mace'
+               this.setState({char: char})
+               break;
+            case 'h': 
+               console.log('nommed health');
+               arr.splice(i, 1) // delete from arr
+               char.health += 10;
+               this.setState({char: char})
+               break;
+            case 'e': 
+               // arr.splice(arr.indexOf(i), 1) // delete from arr
+               console.log('nommed enemy');
+               char.xp += 100;
+               this.setState({char: char})
+               //TODO: deal with enemy encounter. currently gets nommed but doesnt disappear
+               break;
             }
          }
       });
@@ -301,8 +447,6 @@ let Main = React.createClass({
       } else {
          y = cc[indexToChange];
       }
-      
-
 
       grid[x][y] = 1;
    },
@@ -319,8 +463,12 @@ let Main = React.createClass({
 
    renderRows(e, i) {
       let {grid, charCoords, generatedCells} = this.state;
-      let {weapons, healths, blocks, paths, goal} = generatedCells;
+      let {weapons, healths, blocks, paths, enemies, goal} = generatedCells;
+      let enemyCoords = [];
 
+      generatedCells.enemies.forEach((e, i) => {
+         enemyCoords.push(e.coords)
+      })
       let classToAdd = '';
 
       let tds = e.map((l, j) => {
@@ -333,7 +481,7 @@ let Main = React.createClass({
             classToAdd = 'healthCell'; // render a health cell
          } else if(goal == `${i}_${j}`) {
             classToAdd = 'goalCell'; // render the goal cell
-         } else if(goal == `${i}_${j}`) {
+         } else if(enemyCoords.indexOf(`${i}_${j}`) !== -1) {
             classToAdd = 'enemyCell'; // render the enemy cell
          } else if(blocks.indexOf(`${i}_${j}`) !== -1) {
             classToAdd = 'blockCell'; // render a block cell
@@ -355,16 +503,37 @@ let Main = React.createClass({
    render(){
       let loading = this.state.grid.length === 0;
       return (
-         <table>
-            <tbody>
-               {loading ? <Loading /> : this.renderTable()}
-            </tbody>
-         </table>
+         <div>
+            <table>
+               <tbody>
+                  {loading ? <Loading /> : this.renderTable()}
+               </tbody>
+            </table>
+            <UserInterface state={this.state}/>
+         </div>
 
       );
    }
 });
 
-let Loading = () => (<div></div>);
+let UserInterface = React.createClass({
+   render(){
+      let {char, level} = this.props.state
+      console.log(char)
+      return (
+         <div>
+            <span>Level: {level}</span>
+            <span> . . . . . . . . . </span>
+            <span>Health: {char.health}</span>
+            <span> . . . . . . . . . </span>
+            <span>XP: {char.xp}</span>
+            <span> . . . . . . . . . </span>
+            <span>Weapon: {char.weapon}</span>
+         </div>
+      )
+   }
+})
+
+let Loading = () => (<div>Loading...</div>);
 
 ReactDOM.render(<Main />, document.getElementById('root'));
